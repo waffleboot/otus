df02203ad3dadf32df4fe830020184a3be7acf5a
Andrei Yangabishev <waffleboot@gmail.com>
2023-11-09 22:03:49 +0300

    main.tf

--- dz1/main.tf
+++ dz1/main.tf
@@ -10,3 +10,28 @@ terraform {
 provider "yandex" {
   zone = "ru-central1-a"
 }
+
+resource "yandex_compute_instance" "vm-1" {
+  name = "terraform1"
+  resources {
+    cores = 2
+    memory = 4
+  }
+  boot_disk {
+    initialize_params {
+        image_id = "fd826honb8s0i1jtt6cg"
+    }
+  }
+  network_interface {
+    subnet_id = "${yandex_vpc_subnet.foo.id}"
+  }
+}
+
+resource "yandex_vpc_subnet" "foo" {
+  zone           = "ru-central1-a"
+  network_id     = "${yandex_vpc_network.foo.id}"
+  v4_cidr_blocks = ["10.5.0.0/24"]
+}
+
+resource "yandex_vpc_network" "foo" {}
+
