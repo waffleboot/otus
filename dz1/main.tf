2a7cebf1b08c9a6531025c120fa217fd2aa489b6
Andrei Yangabishev <waffleboot@gmail.com>
2023-11-10 18:58:42 +0300

    убрать кавычки

--- dz1/main.tf
+++ dz1/main.tf
@@ -7,19 +7,19 @@ terraform {
   required_version = ">= 0.13"
 }
 
-provider "yandex" {
+provider yandex {
   zone = "ru-central1-a"
 }
 
-resource "yandex_vpc_network" "nginx" {}
+resource yandex_vpc_network nginx {}
 
-resource "yandex_vpc_subnet" "nginx" {
-  network_id     = "${yandex_vpc_network.nginx.id}"
+resource yandex_vpc_subnet nginx {
+  network_id     = yandex_vpc_network.nginx.id
   v4_cidr_blocks = ["192.168.0.0/16"]
   zone           = "ru-central1-a"
 }
 
-resource "yandex_compute_instance" "nginx" {
+resource yandex_compute_instance nginx {
   resources {
     cores = 2
     memory = 4
@@ -30,39 +30,39 @@ resource "yandex_compute_instance" "nginx" {
     }
   }
   network_interface {
-    subnet_id = "${yandex_vpc_subnet.nginx.id}"
+    subnet_id = yandex_vpc_subnet.nginx.id
     nat = true
   }
   metadata = {
     ssh-keys = "ubuntu:${tls_private_key.key.public_key_openssh}"
   }
-  provisioner "remote-exec" {
+  provisioner remote-exec {
     inline = ["true"]
     connection {
       type = "ssh"
       user = "ubuntu"
-      host = "${self.network_interface.0.nat_ip_address}"
-      private_key = "${tls_private_key.key.private_key_pem}"
+      host = self.network_interface.0.nat_ip_address
+      private_key = tls_private_key.key.private_key_pem
     }
   }
-  provisioner "local-exec" {
+  provisioner local-exec {
     command = "ansible-playbook -u ubuntu -i '${self.network_interface.0.nat_ip_address},' --private-key id_rsa playbook.yml"
     environment = {
-      ANSIBLE_HOST_KEY_CHECKING = "False"
+      ANSIBLE_HOST_KEY_CHECKING: "False"
     }
   }
-  provisioner "local-exec" {
+  provisioner local-exec {
     command = "curl http://${self.network_interface.0.nat_ip_address}"
   }
 }
 
-resource "tls_private_key" "key" {
+resource tls_private_key key {
   algorithm = "RSA"
   rsa_bits = 4096
-  provisioner "local-exec" {
+  provisioner local-exec {
     command = "echo '${self.private_key_pem}' > id_rsa"
   }
-  provisioner "local-exec" {
+  provisioner local-exec {
     command = "chmod og-rwx id_rsa"
   }
 }
