8d2c7087fe13858a17b77faa8a795466b08412a2
Andrei Yangabishev <waffleboot@gmail.com>
2023-11-16 20:19:01 +0300

    добавить docker entry

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
+.DEFAULT_GOAL := run
+
 .PHONY: build
 build:
 	@docker build -t otus .
---
+++ init.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export YC_TOKEN=$(yc iam create-token)
+export YC_CLOUD_ID=$(yc config get cloud-id)
+export YC_FOLDER_ID=$(yc config get folder-id)
