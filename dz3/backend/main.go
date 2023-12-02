570f8b570c8a63645f4c62dd1d4b379d84bbb794
Andrei Yangabishev <waffleboot@gmail.com>
2023-12-02 22:42:53 +0300

    показывать статику

--- dz3/backend/main.go
+++ dz3/backend/main.go
@@ -15,33 +15,50 @@ import (
 )
 
 func main() {
-	if len(os.Args) < 3 {
-		fmt.Println("app <dbUrl> <httpPort>")
+	err := runApp()
+	if err != nil {
+		fmt.Println(err)
 		os.Exit(1)
+	}
+}
+
+func runApp() error {
+	if len(os.Args) < 4 {
+		return errors.New("app <dbUrl> <httpPort> <staticDir>")
 	}
 
 	dbUrl := os.Args[1]
 	httpPort := os.Args[2]
+	staticDir := os.Args[3]
+
+	err := checkStaticDir(staticDir)
+	if err != nil {
+		return fmt.Errorf("check static dir: %w", err)
+	}
 
 	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT)
 	defer cancel()
 
-	err := run(ctx, dbUrl, httpPort)
+	err = run(ctx, dbUrl, httpPort, staticDir)
 	if err != nil {
-		fmt.Println(err)
+		return fmt.Errorf("run: %w", err)
 	}
+
+	return nil
 }
 
-func run(ctx context.Context, dbUrl, httpPort string) error {
+func run(ctx context.Context, dbUrl, httpPort, staticDir string) error {
 	dbpool, err := initPostgres(ctx, dbUrl)
 	if err != nil {
 		return fmt.Errorf("init postgres: %w", err)
 	}
 	defer dbpool.Close()
 
+	http.Handle("/db", http.HandlerFunc(dbHandler(dbpool)))
+	http.Handle("/", http.FileServer(http.Dir(staticDir)))
+
 	server := &http.Server{
-		Addr:    ":" + httpPort,
-		Handler: httpHandler(dbpool),
+		Addr: ":" + httpPort,
 	}
 
 	g, gCtx := errgroup.WithContext(ctx)
@@ -98,7 +115,7 @@ func initPostgres(ctx context.Context, url string) (*pgxpool.Pool, error) {
 	return dbpool, nil
 }
 
-func httpHandler(dbpool *pgxpool.Pool) http.HandlerFunc {
+func dbHandler(dbpool *pgxpool.Pool) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 		conn, err := dbpool.Acquire(r.Context())
@@ -119,4 +136,22 @@ func httpHandler(dbpool *pgxpool.Pool) http.HandlerFunc {
 		w.WriteHeader(http.StatusOK)
 		_, _ = fmt.Fprintln(w, ts)
 	})
+}
+
+func checkStaticDir(staticDir string) error {
+	f, err := os.Open(staticDir)
+	if err != nil {
+		return fmt.Errorf("open: %w", err)
+	}
+
+	fi, err := f.Stat()
+	if err != nil {
+		return fmt.Errorf("stat: %w", err)
+	}
+
+	if !fi.IsDir() {
+		return errors.New("static dir is not dir")
+	}
+
+	return nil
 }
