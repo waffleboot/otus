df01837522f045e4aa24a0b72ca9290584e311f2
Andrei Yangabishev <waffleboot@gmail.com>
2023-12-06 21:56:05 +0300

    поменять блок ip адресов

--- dz3/main.tf
+++ dz3/main.tf
@@ -15,7 +15,7 @@ resource yandex_vpc_network net {}
 
 resource yandex_vpc_subnet subnet {
   network_id     = yandex_vpc_network.net.id
-  v4_cidr_blocks = ["192.168.0.0/16"]
+  v4_cidr_blocks = ["192.168.0.0/24"]
   zone           = "ru-central1-a"
   route_table_id = yandex_vpc_route_table.rt.id
 }
