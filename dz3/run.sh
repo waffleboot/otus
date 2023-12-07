4ec1fed0c10406b44e22980cd12e4449fc984b41
Andrei Yangabishev <waffleboot@gmail.com>
2023-12-07 23:27:30 +0300

    wip

--- dz3/run.sh
+++ dz3/run.sh
@@ -6,27 +6,27 @@ case $1 in
             nginx)
                 case $3 in
                     all)
-                        ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos@nginx-1.ru-central1.internal -oProxyCommand="ssh centos@84.201.159.47 -i id_rsa -W %h:%p" -i id_rsa sudo systemctl stop nginx
-                        ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos@nginx-2.ru-central1.internal -oProxyCommand="ssh centos@84.201.159.47 -i id_rsa -W %h:%p" -i id_rsa sudo systemctl stop nginx
+                        ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos@nginx-1.ru-central1.internal -oProxyCommand="ssh centos@62.84.116.217 -i id_rsa -W %h:%p" -i id_rsa sudo systemctl stop nginx
+                        ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos@nginx-2.ru-central1.internal -oProxyCommand="ssh centos@62.84.116.217 -i id_rsa -W %h:%p" -i id_rsa sudo systemctl stop nginx
                         ;;
                     *)
-                        ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos@nginx-$3.ru-central1.internal -oProxyCommand="ssh centos@84.201.159.47 -i id_rsa -W %h:%p" -i id_rsa sudo systemctl stop nginx
+                        ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos@nginx-$3.ru-central1.internal -oProxyCommand="ssh centos@62.84.116.217 -i id_rsa -W %h:%p" -i id_rsa sudo systemctl stop nginx
                         ;;
                 esac
                 ;;
             back)
                 case $3 in
                     all)
-                        ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos@backend-1.ru-central1.internal -oProxyCommand="ssh centos@84.201.159.47 -i id_rsa -W %h:%p" -i id_rsa sudo docker stop wordpress
-                        ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos@backend-2.ru-central1.internal -oProxyCommand="ssh centos@84.201.159.47 -i id_rsa -W %h:%p" -i id_rsa sudo docker stop wordpress
+                        ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos@backend-1.ru-central1.internal -oProxyCommand="ssh centos@62.84.116.217 -i id_rsa -W %h:%p" -i id_rsa sudo docker stop wordpress
+                        ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos@backend-2.ru-central1.internal -oProxyCommand="ssh centos@62.84.116.217 -i id_rsa -W %h:%p" -i id_rsa sudo docker stop wordpress
                         ;;
                     *)
-                        ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos@backend-$3.ru-central1.internal -oProxyCommand="ssh centos@84.201.159.47 -i id_rsa -W %h:%p" -i id_rsa sudo docker stop wordpress
+                        ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos@backend-$3.ru-central1.internal -oProxyCommand="ssh centos@62.84.116.217 -i id_rsa -W %h:%p" -i id_rsa sudo docker stop wordpress
                         ;;
                 esac
                 ;;
             bastion)
-                ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos@bastion.ru-central1.internal -oProxyCommand="ssh centos@84.201.159.47 -i id_rsa -W %h:%p" -i id_rsa sudo systemctl stop nginx
+                ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos@bastion.ru-central1.internal -oProxyCommand="ssh centos@62.84.116.217 -i id_rsa -W %h:%p" -i id_rsa sudo systemctl stop nginx
                 ;;
         esac
         ;;
@@ -35,47 +35,47 @@ case $1 in
             nginx)
                 case $3 in
                     all)
-                        ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos@nginx-1.ru-central1.internal -oProxyCommand="ssh centos@84.201.159.47 -i id_rsa -W %h:%p" -i id_rsa sudo systemctl start nginx
-                        ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos@nginx-2.ru-central1.internal -oProxyCommand="ssh centos@84.201.159.47 -i id_rsa -W %h:%p" -i id_rsa sudo systemctl start nginx
+                        ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos@nginx-1.ru-central1.internal -oProxyCommand="ssh centos@62.84.116.217 -i id_rsa -W %h:%p" -i id_rsa sudo systemctl start nginx
+                        ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos@nginx-2.ru-central1.internal -oProxyCommand="ssh centos@62.84.116.217 -i id_rsa -W %h:%p" -i id_rsa sudo systemctl start nginx
                         ;;
                     *)
-                        ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos@nginx-$3.ru-central1.internal -oProxyCommand="ssh centos@84.201.159.47 -i id_rsa -W %h:%p" -i id_rsa sudo systemctl start nginx
+                        ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos@nginx-$3.ru-central1.internal -oProxyCommand="ssh centos@62.84.116.217 -i id_rsa -W %h:%p" -i id_rsa sudo systemctl start nginx
                         ;;
                 esac
                 ;;
             back)
                 case $3 in
                     all)
-                        ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos@backend-1.ru-central1.internal -oProxyCommand="ssh centos@84.201.159.47 -i id_rsa -W %h:%p" -i id_rsa sudo docker start wordpress
-                        ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos@backend-2.ru-central1.internal -oProxyCommand="ssh centos@84.201.159.47 -i id_rsa -W %h:%p" -i id_rsa sudo docker start wordpress
+                        ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos@backend-1.ru-central1.internal -oProxyCommand="ssh centos@62.84.116.217 -i id_rsa -W %h:%p" -i id_rsa sudo docker start wordpress
+                        ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos@backend-2.ru-central1.internal -oProxyCommand="ssh centos@62.84.116.217 -i id_rsa -W %h:%p" -i id_rsa sudo docker start wordpress
                         ;;
                     *)
-                        ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos@backend-$3.ru-central1.internal -oProxyCommand="ssh centos@84.201.159.47 -i id_rsa -W %h:%p" -i id_rsa sudo docker start wordpress
+                        ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos@backend-$3.ru-central1.internal -oProxyCommand="ssh centos@62.84.116.217 -i id_rsa -W %h:%p" -i id_rsa sudo docker start wordpress
                         ;;
                 esac
                 ;;
             bastion)
-                ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos@bastion.ru-central1.internal -oProxyCommand="ssh centos@84.201.159.47 -i id_rsa -W %h:%p" -i id_rsa sudo systemctl start nginx
+                ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos@bastion.ru-central1.internal -oProxyCommand="ssh centos@62.84.116.217 -i id_rsa -W %h:%p" -i id_rsa sudo systemctl start nginx
                 ;;
         esac
         ;;
     ssh)
         case $2 in
             nginx)
-                ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos@nginx-$3.ru-central1.internal -oProxyCommand="ssh centos@84.201.159.47 -i id_rsa -W %h:%p" -i id_rsa
+                ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos@nginx-$3.ru-central1.internal -oProxyCommand="ssh centos@62.84.116.217 -i id_rsa -W %h:%p" -i id_rsa
                 ;;
             back)
-                ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos@backend-$3.ru-central1.internal -oProxyCommand="ssh centos@84.201.159.47 -i id_rsa -W %h:%p" -i id_rsa
+                ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos@backend-$3.ru-central1.internal -oProxyCommand="ssh centos@62.84.116.217 -i id_rsa -W %h:%p" -i id_rsa
                 ;;
             db)
-                ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos@db.ru-central1.internal -oProxyCommand="ssh centos@84.201.159.47 -i id_rsa -W %h:%p" -i id_rsa
+                ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos@db.ru-central1.internal -oProxyCommand="ssh centos@62.84.116.217 -i id_rsa -W %h:%p" -i id_rsa
                 ;;
             bastion)
-                ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos@bastion.ru-central1.internal -oProxyCommand="ssh centos@84.201.159.47 -i id_rsa -W %h:%p" -i id_rsa
+                ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos@bastion.ru-central1.internal -oProxyCommand="ssh centos@62.84.116.217 -i id_rsa -W %h:%p" -i id_rsa
                 ;;
         esac
         ;;
     open)
-        open http://84.201.159.47
+        open http://62.84.116.217
         ;;
 esac
